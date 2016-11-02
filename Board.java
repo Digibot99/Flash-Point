490988ffa816069cf372dd97027c449d988667c2
Digibot99 <wheelersc@cox.net>
2016-11-01 21:36:08 -0700

    Completed Walls began Smoke

---
+++ Board.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flashpoint;
+
+/**
+ *
+ * @author 384000346
+ */
+public class Board {
+//    final static int numRows = 6;
+//    final static int numColumns = 8;
+    final static int numRows = 8;
+    final static int numColumns = 10;
+    final static int EMPTY = 0;
+    final static int PLAYERRED = 1;
+    final static int PLAYERBLUE = 2;
+    final static int PLAYERGREEN = 3;
+    final static int PLAYERYELLOW = 4;
+    final static int SMOKE = 5;
+    final static int FIRE = 6;
+    final static int WALL = 7;
+    final static int DOOR = 8;
+    static int board[][];
+    
+}
---
+++ Player.java
@@ -0,0 +1,62 @@
+package flashpoint;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.lang.Math;
+
+public class Player {
+//    public enum Players{
+//        Red,Blue,Green,Yellow
+//    }
+    private int actionPoints;
+    int currentRow;
+    int currentColumn;
+    private Color color;
+    private boolean isTurn;
+    Player(Color _color){
+        actionPoints = 0;
+        color = _color;
+        currentColumn = (int)(Math.random()*Board.numColumns);
+        currentRow = (int)(Math.random()*Board.numRows);
+        }
+    public int getActionPoints()
+    {
+        return(actionPoints);
+    }
+    public int setActionPoints()
+    {
+        actionPoints = (int)(Math.random() * 4 + 1);
+        return(actionPoints);
+    }
+    public void playerLoseActionPoint()
+    {
+        actionPoints -= 1;
+    }
+    public Color getColor ()
+    {
+        return(color);
+    }
+    public int getCurrentRow ()
+    {
+        return(currentRow);
+    }
+    public int getCurrentColumn ()
+    {
+        return(currentColumn);
+    }
+    public void setCurrentRow (int _currentRow)
+    {
+        currentRow = _currentRow;
+    }
+    public void setCurrentColumn (int _currentColumn)
+    {
+        currentColumn = _currentColumn;
+    }
+    public boolean getisTurn ()
+    {
+        return(isTurn);
+    }
+    public void setisTurn (boolean _temp)
+    {
+        isTurn = _temp;
+    }
+}
